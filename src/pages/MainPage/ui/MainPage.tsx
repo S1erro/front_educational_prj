b5bae0f035b4480c05602a37ec1bf86cceba3be8
S1erro <riviev92@mail.ru>
2024-07-09 17:11:31 +0300

    добавлены плагин для переводов i18n, разбиение на чанки, свитчер языкка

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const MainPage = () => {
+    const {t} = useTranslation('translation');
+
     return (
         <div>
-            MainPAGE
+            {t('Главная страница')}
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import * as cls from './Sidebar.module.scss'
 import React, {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -18,7 +19,8 @@ export const Sidebar = ({className}: SidebarProps) => {
         <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
-                <ThemeSwitcher/>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
